4daf70ae451402cb490044884f1f0c43aa8d0072
wangzhengzheng <wangzhengzheng@delanshi.net>
2020-09-25 09:50:51 +0800

    fix router

--- src/config/router.config.js
+++ src/config/router.config.js
@@ -2757,8 +2757,7 @@ export const asyncRouterMap = [
           permission: ['permission']
         },
         redirect: '/vehicle-management/car-info-list',
-        children: [
-          {
+        children: [{
             path: '/vehicle-management/car-info-list',
             name: 'vehicle-management-car-info-list',
             component: () => import('@/views/vehicle-management/car-info-list/List'),
@@ -2875,9 +2874,67 @@ export const asyncRouterMap = [
               }
             ]
           }
-
         ]
       },
+
+      {
+        //应急管理
+        path: '/contingency-management',
+        name: 'contingency-management',
+        component: PageView,
+        meta: {
+          title: '应急管理',
+          icon: 'shop',
+          permission: ['permission']
+        },
+        redirect: '/contingency-management/medicines',
+        children: [
+          {
+            path: '/contingency-management/medicines',
+            name: 'contingency-management-medicines',
+            component: () => import('@/views/contingency-management/medicines/List'),
+            meta: {
+              title: '药品物资管理',
+              icon: 'select',
+              keepAlive: false,
+              permission: ['permission']
+            },
+          },
+          {
+            path: '/contingency-management/medicines-cabinet-config',
+            name: 'contingency-management-medicines-cabinet-config',
+            component: () => import('@/views/contingency-management/medicines-cabinet-config/List'),
+            meta: {
+              title: '药品柜类型',
+              icon: 'select',
+              keepAlive: false,
+              permission: ['permission']
+            },
+          },
+          {
+            path: '/contingency-management/medicines-cabinet',
+            name: 'contingency-management-medicines-cabinet',
+            component: () => import('@/views/contingency-management/medicines-cabinet/List'),
+            meta: {
+              title: '药品柜',
+              icon: 'select',
+              keepAlive: false,
+              permission: ['permission']
+            },
+          },
+          {
+            path: '/contingency-management/medicines-record',
+            name: 'contingency-management-medicines-record',
+            component: () => import('@/views/contingency-management/medicines-record/List'),
+            meta: {
+              title: '领取记录',
+              icon: 'select',
+              keepAlive: false,
+              permission: ['permission']
+            },
+          },
+        ]
+      }
 
       
     ]
