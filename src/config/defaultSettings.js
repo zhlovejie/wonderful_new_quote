d0818585eb029523c0b8cb2abd5e6d1c47072ad5
xu520 <18260703019@163.com>
2020-10-12 13:16:10 +0800

    修改高拍仪上传

--- src/config/defaultSettings.js
+++ src/config/defaultSettings.js
@@ -31,12 +31,12 @@ export default {
   //baseURL: 'http://192.168.13.133:8995', // 玉堂
   // baseURL: 'http://192.168.13.27:8999/oa', // 王利
   // baseURL: 'http://192.168.13.197:8999/oa', // 吴远公告
-  baseURL: 'http://192.168.13.197:8995/cloud_new', // 吴远 薪酬
+  // baseURL: 'http://192.168.13.197:8995/cloud_new', // 吴远 薪酬
 
   //baseURL: 'http://106.14.194.64/cloud_new',
   //baseURL: 'http://192.168.13.250/cloud',
   // baseURL: 'http://192.168.13.101:8999/oa', // 王利
-  //baseURL:'http://192.168.13.253/oa',
+  baseURL: 'http://192.168.13.253/oa',
 
   gaoPaiYiUploadProxyURL: 'http://localhost:8090/', //参考 GaoPaiYiUploadProxy.exe.config -> localServerUrl
   primaryColor: '#1890FF', // primary color of ant design
