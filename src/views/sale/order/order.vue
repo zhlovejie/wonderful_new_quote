f1358f496ca651dda97159daf8d17d691f548a31
yuyutang <yyt@delanshi.net>
2020-11-12 13:45:40 +0800

    销售订单相关修改

--- src/views/sale/order/order.vue
+++ src/views/sale/order/order.vue
@@ -132,12 +132,6 @@ const columns = [
   },
   {
     align: 'center',
-    title: '预定最近交货日期',
-    dataIndex: 'latelyDeliveryDate',
-    key: 'latelyDeliveryDate'
-  },
-  {
-    align: 'center',
     title: '创建人',
     key: 'createdName',
     dataIndex: 'createdName'
