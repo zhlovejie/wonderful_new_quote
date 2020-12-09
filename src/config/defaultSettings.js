f4d3620c74d4c934845b19352681299f3ccab391
xu520 <18260703019@163.com>
2020-12-09 10:29:21 +0800

    完成外出申请模块

--- src/config/defaultSettings.js
+++ src/config/defaultSettings.js
@@ -16,10 +16,10 @@
 export default {
   // baseURL: 'http://106.14.246.246:8091',    // 正式服
   // baseURL: 'http://192.168.13.158:8999/oa',
-  baseURL: 'http://192.168.13.2:8997/oa',
-  // baseURL: '  http://192.168.13.138:8998',
+  // baseURL: 'http://192.168.13.2:8999/oa',
+  baseURL: '  http://192.168.13.138:8999',
   // baseURL: 'http://192.168.13.158:8997/oa',
-
+  // baseURL: 'http://192.168.13.250/cloud',
 
 
   // baseURL: 'http://192.168.13.253/oa',
