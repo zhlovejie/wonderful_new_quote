67a3bc7ac8758bc6d80c4b20af075f6cc9a562aa
xu520 <18260703019@163.com>
2020-12-14 15:07:16 +0800

    修改核价单销售人员不显示问题

--- src/views/sales-management/pricing-module/pricings/SearchForm.vue
+++ src/views/sales-management/pricing-module/pricings/SearchForm.vue
@@ -12,29 +12,34 @@
         <a-col :span="24">基本信息</a-col>
         <a-col :span="12">
           <a-form-item label="客户名称">
-            <a-input v-decorator="['customerName']" placeholder="客户名称" :allowClear="true"/>
+            <a-input v-decorator="['customerName']" placeholder="客户名称" :allowClear="true" />
           </a-form-item>
         </a-col>
         <a-col :span="12">
           <a-form-item label="销售经理">
             <a-select placeholder="销售经理" v-decorator="['saleUserId']">
-              <a-select-option v-for="salesman in salesJurisdiction.subSalesman" :key="salesman.id" :value="salesman.userId">{{ salesman.salesmanName }}</a-select-option>
+              <a-select-option
+                v-for="salesman in salesJurisdiction.allSalesman"
+                :key="salesman.id"
+                :value="salesman.userId"
+                >{{ salesman.salesmanName }}</a-select-option
+              >
             </a-select>
           </a-form-item>
         </a-col>
         <a-col :span="12">
           <a-form-item label="提交人">
-            <a-input v-decorator="['createName']" placeholder="提交人" :allowClear="true"/>
+            <a-input v-decorator="['createName']" placeholder="提交人" :allowClear="true" />
           </a-form-item>
         </a-col>
         <a-col :span="12">
           <a-form-item label="核价编号">
-            <a-input v-decorator="['valencyCode']" placeholder="核价编号" :allowClear="true"/>
+            <a-input v-decorator="['valencyCode']" placeholder="核价编号" :allowClear="true" />
           </a-form-item>
         </a-col>
         <a-col :span="12">
           <a-form-item label="单据状态">
-            <a-select v-decorator="['approveStatus',{initialValue:-1}]" placeholder="单据状态">
+            <a-select v-decorator="['approveStatus', { initialValue: -1 }]" placeholder="单据状态">
               <a-select-option :value="-1">请选择审批状态</a-select-option>
               <a-select-option :value="0">待接收</a-select-option>
               <a-select-option :value="1">已接收待核价</a-select-option>
@@ -47,46 +52,45 @@
         </a-col>
         <a-col :span="12">
           <a-form-item label="提交时间">
-            <a-range-picker v-decorator="['sDate']" style="width:100%;" :allowClear="true"/>
+            <a-range-picker v-decorator="['sDate']" style="width: 100%" :allowClear="true" />
           </a-form-item>
         </a-col>
-        
       </a-row>
       <a-row :gutter="0">
         <a-col :span="24">核价信息</a-col>
         <a-col :span="12">
           <a-form-item label="产品名称">
-            <a-input v-decorator="['productName']" placeholder="产品名称" :allowClear="true"/>
+            <a-input v-decorator="['productName']" placeholder="产品名称" :allowClear="true" />
           </a-form-item>
         </a-col>
         <a-col :span="12">
           <a-form-item label="规格型号">
-            <a-input v-decorator="['valencySpecs']" placeholder="规格型号" :allowClear="true"/>
+            <a-input v-decorator="['valencySpecs']" placeholder="规格型号" :allowClear="true" />
           </a-form-item>
         </a-col>
         <a-col :span="12">
           <a-form-item label="产品代码">
-            <a-input v-decorator="['newBasisModel']" placeholder="产品代码" :allowClear="true"/>
+            <a-input v-decorator="['newBasisModel']" placeholder="产品代码" :allowClear="true" />
           </a-form-item>
         </a-col>
         <a-col :span="12">
           <a-form-item label="依据产品名称">
-            <a-input v-decorator="['toolProductName']" placeholder="依据产品名称" :allowClear="true"/>
+            <a-input v-decorator="['toolProductName']" placeholder="依据产品名称" :allowClear="true" />
           </a-form-item>
         </a-col>
         <a-col :span="12">
           <a-form-item label="依据规格型号">
-            <a-input v-decorator="['toolProductStandard']" placeholder="依据规格型号" :allowClear="true"/>
+            <a-input v-decorator="['toolProductStandard']" placeholder="依据规格型号" :allowClear="true" />
           </a-form-item>
         </a-col>
         <a-col :span="12">
           <a-form-item label="依据产品代码">
-            <a-input v-decorator="['toolProductModel']" placeholder="依据产品代码" :allowClear="true"/>
+            <a-input v-decorator="['toolProductModel']" placeholder="依据产品代码" :allowClear="true" />
           </a-form-item>
         </a-col>
         <a-col :span="24">
           <a-form-item label="需求日期">
-            <a-range-picker v-decorator="['dDate']" style="width:100%;" />
+            <a-range-picker v-decorator="['dDate']" style="width: 100%" />
           </a-form-item>
         </a-col>
       </a-row>
@@ -105,25 +109,24 @@
   </a-modal>
 </template>
 <script>
-import { salesJurisdiction} from '@/api/customer'
+import { salesJurisdiction } from '@/api/customer'
 export default {
   name: 'searchForm',
-  components:{
-  },
+  components: {},
   data() {
     return {
       modalTitle: '高级筛选',
       visible: false,
       form: this.$form.createForm(this),
-      salesJurisdiction:[]
+      salesJurisdiction: {},
     }
   },
   methods: {
     query() {
       let that = this
       that.visible = true
-      that.$nextTick(() =>{
-        salesJurisdiction().then(res => that.salesJurisdiction = res.data)
+      that.$nextTick(() => {
+        salesJurisdiction().then((res) => (that.salesJurisdiction = res.data))
       })
     },
     handleCancel() {
@@ -136,12 +139,12 @@ export default {
         this.form.resetFields()
       } else if (type === 'search') {
         let values = this.form.getFieldsValue()
-        if(Array.isArray(values.sDate) && values.sDate.length === 2){
+        if (Array.isArray(values.sDate) && values.sDate.length === 2) {
           values.startTime = values.sDate[0].format('YYYY-MM-DD')
           values.endTime = values.sDate[1].format('YYYY-MM-DD')
           delete values.sDate
         }
-        if(Array.isArray(values.dDate) && values.dDate.length === 2){
+        if (Array.isArray(values.dDate) && values.dDate.length === 2) {
           values.startDemandTime = values.dDate[0].format('YYYY-MM-DD')
           values.endDemandTime = values.dDate[1].format('YYYY-MM-DD')
           delete values.dDate
@@ -150,21 +153,20 @@ export default {
         this.$emit('change', values)
         this.handleCancel()
       }
-    }
-  }
+    },
+  },
 }
 </script>
 <style scoped>
-.search-form-wrapper >>> .ant-form-item{
+.search-form-wrapper >>> .ant-form-item {
   display: flex;
 }
-.search-form-wrapper >>> .ant-form-item-control-wrapper{
+.search-form-wrapper >>> .ant-form-item-control-wrapper {
   flex: 1;
 }
-.search-form-wrapper >>> .ant-form-item-label{
+.search-form-wrapper >>> .ant-form-item-label {
   display: inline-block;
   width: 120px;
   padding-left: 20px;
 }
-
 </style>
