9d41b4060b18f7d730fe27ebc135b1720d3ceb38
wangzhengzheng <wangzhengzheng@delanshi.net>
2021-01-07 16:46:17 +0800

    fix 考勤统计 添加 调休列，移除 外出 列

--- src/views/attendance-management/chart/AttendanceStatisticsList.vue
+++ src/views/attendance-management/chart/AttendanceStatisticsList.vue
@@ -115,13 +115,19 @@ const columns = [
     dataIndex: 'leaveHours',
     key: 'leaveHours',
   },
-
   {
     align: 'center',
-    title: '外出（h）',
-    dataIndex: 'egressLeaveHours',
-    key: 'egressLeaveHours',
+    title: '调休（h）',
+    dataIndex: 'leaveWithRestHours',
+    key: 'leaveWithRestHours',
   },
+
+  // {
+  //   align: 'center',
+  //   title: '外出（h）',
+  //   dataIndex: 'egressLeaveHours',
+  //   key: 'egressLeaveHours',
+  // },
 
   {
     align: 'center',
