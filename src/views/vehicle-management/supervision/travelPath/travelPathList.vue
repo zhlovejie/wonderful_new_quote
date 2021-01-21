b74e8081309bf245fb7ecfafa47ada788de747e5
xu520 <18260703019@163.com>
2021-01-21 14:12:15 +0800

    完成行程轨迹

---
+++ src/views/vehicle-management/supervision/travelPath/travelPathList.vue
@@ -0,0 +1,31 @@
+<template>
+  <a-card :bordered="false">
+    <!--行程轨迹跳转-->
+    <div class="paths">
+      此页面使用第三方地址
+      <a
+        href="http://218.244.129.243:89/mygpsonline/gpsonline/jsp/login.jsp?jsonmsg=%E8%B4%A6%E5%8F%B7%E5%AF%86%E7%A0%81%E9%94%99%E8%AF%AF%EF%BC%81  "
+        target="_blank"
+        >点击跳转</a
+      >
+    </div>
+  </a-card>
+</template>
+
+<script>
+export default {
+  name: '',
+  components: {},
+  data() {
+    return {}
+  },
+  created() {},
+  methods: {},
+}
+</script>
+<style >
+.paths {
+  text-align: center;
+  font-size: 20px;
+}
+</style>
