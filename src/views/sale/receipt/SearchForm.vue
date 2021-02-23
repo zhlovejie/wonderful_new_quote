a81afa0253e5002562572334cd02620d34cda526
xu520 <18260703019@163.com>
2021-02-23 13:41:36 +0800

    修改收款单高级搜索没有已撤回搜索

--- src/views/sale/receipt/SearchForm.vue
+++ src/views/sale/receipt/SearchForm.vue
@@ -18,7 +18,7 @@
               :allowClear="true"
               :filterOption="filterSalersOption"
               placeholder="销售经理"
-              style="width: 100%;"
+              style="width: 100%"
               v-decorator="['saleUserId']"
             >
               <a-select-option v-for="item in saleUser" :value="item.userId" :key="item.userId">{{
@@ -29,42 +29,43 @@
         </a-col>
         <a-col :span="12">
           <a-form-item label="客户名称">
-            <a-input v-decorator="['customerName']" placeholder="客户名称" :allowClear="true"/>
+            <a-input v-decorator="['customerName']" placeholder="客户名称" :allowClear="true" />
           </a-form-item>
         </a-col>
         <a-col :span="12">
           <a-form-item label="提交人">
-            <a-input v-decorator="['createName']" placeholder="提交人" :allowClear="true"/>
+            <a-input v-decorator="['createName']" placeholder="提交人" :allowClear="true" />
           </a-form-item>
         </a-col>
         <a-col :span="12">
           <a-form-item label="收款编号">
-            <a-input v-decorator="['receiptCode']" placeholder="收款编号" :allowClear="true"/>
+            <a-input v-decorator="['receiptCode']" placeholder="收款编号" :allowClear="true" />
           </a-form-item>
         </a-col>
         <a-col :span="12">
           <a-form-item label="合同编号">
-            <a-input v-decorator="['contractNum']" placeholder="合同编号" :allowClear="true"/>
+            <a-input v-decorator="['contractNum']" placeholder="合同编号" :allowClear="true" />
           </a-form-item>
         </a-col>
         <a-col :span="24">
           <a-form-item label="提交时间">
-            <a-range-picker v-decorator="['sDate']" style="width:100%;" :allowClear="true"/>
+            <a-range-picker v-decorator="['sDate']" style="width: 100%" :allowClear="true" />
           </a-form-item>
         </a-col>
         <a-col :span="12">
           <a-form-item label="单据状态">
-            <a-select v-decorator="['approveStatus',{initialValue:0}]" placeholder="单据状态">
+            <a-select v-decorator="['approveStatus', { initialValue: 0 }]" placeholder="单据状态">
               <a-select-option :value="0">全部</a-select-option>
               <a-select-option :value="1">待审批</a-select-option>
               <a-select-option :value="2">通过</a-select-option>
-              <a-select-option :value="3">不通过</a-select-option>  
+              <a-select-option :value="3">不通过</a-select-option>
+              <a-select-option :value="9">已撤回</a-select-option>
             </a-select>
           </a-form-item>
         </a-col>
         <a-col :span="24">
           <a-form-item label="收款日期">
-            <a-range-picker v-decorator="['dDate']" style="width:100%;" />
+            <a-range-picker v-decorator="['dDate']" style="width: 100%" />
           </a-form-item>
         </a-col>
       </a-row>
@@ -86,14 +87,13 @@
 import { getListSaleContractUser } from '@/api/contractListManagement'
 export default {
   name: 'searchForm',
-  components:{
-  },
+  components: {},
   data() {
     return {
       modalTitle: '高级筛选',
       visible: false,
       form: this.$form.createForm(this),
-      saleUser:[]
+      saleUser: [],
     }
   },
   methods: {
@@ -111,20 +111,20 @@ export default {
         this.form.resetFields()
       } else if (type === 'search') {
         let values = this.form.getFieldsValue()
-        if(Array.isArray(values.sDate) && values.sDate.length === 2){
+        if (Array.isArray(values.sDate) && values.sDate.length === 2) {
           values.startTime = values.sDate[0].format('YYYY-MM-DD')
           values.endTime = values.sDate[1].format('YYYY-MM-DD')
           delete values.sDate
-        }else{
+        } else {
           values.startTime = undefined
           values.endTime = undefined
           delete values.sDate
         }
-        if(Array.isArray(values.dDate) && values.dDate.length === 2){
+        if (Array.isArray(values.dDate) && values.dDate.length === 2) {
           values.receiptStartTime = values.dDate[0].format('YYYY-MM-DD')
           values.receiptEndTime = values.dDate[1].format('YYYY-MM-DD')
           delete values.dDate
-        }else{
+        } else {
           values.receiptStartTime = undefined
           values.receiptEndTime = undefined
           delete values.dDate
@@ -137,19 +137,18 @@ export default {
     filterSalersOption(input, option) {
       return option.componentOptions.children[0].text.toLowerCase().indexOf(input.toLowerCase()) >= 0
     },
-  }
+  },
 }
 </script>
 <style scoped>
-.search-form-wrapper >>> .ant-form-item{
+.search-form-wrapper >>> .ant-form-item {
   display: flex;
 }
-.search-form-wrapper >>> .ant-form-item-control-wrapper{
+.search-form-wrapper >>> .ant-form-item-control-wrapper {
   flex: 1;
 }
-.search-form-wrapper >>> .ant-form-item-label{
+.search-form-wrapper >>> .ant-form-item-label {
   display: inline-block;
   width: 120px;
 }
-
 </style>
