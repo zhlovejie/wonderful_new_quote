c5accfdc87eb1bfa75f9aabdeb26f83e8651d9d4
xu520 <18260703019@163.com>
2021-04-26 10:51:56 +0800

    修改就餐名称

--- src/views/attendance-management/chart/WorkMealsStatisticsList.vue
+++ src/views/attendance-management/chart/WorkMealsStatisticsList.vue
@@ -40,8 +40,8 @@
     </div>
     <div class="main-wrapper">
       <h2>
-        <span>数量(份)：{{ mealInfo.workLunchEatNum || 0 }}</span>
-        <span style="margin-left: 50px">就餐(人)：{{ mealInfo.workLunchNum || 0 }}</span>
+        <span>就餐(份)：{{ mealInfo.workLunchEatNum || 0 }}</span>
+        <span style="margin-left: 50px">未就餐(人)：{{ mealInfo.workLunchNum || 0 }}</span>
       </h2>
       <a-table
         :columns="columns"
