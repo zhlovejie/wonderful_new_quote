d2e9058e211a40ac26f4c1425749f34de378ac0b
wangzhengzheng <wangzhengzheng@delanshi.net>
2021-05-15 16:09:49 +0800

    常规物料库 临时保存

--- src/api/routineMaterial.js
+++ src/api/routineMaterial.js
@@ -250,3 +250,132 @@ export function routineMaterialRuleCopy(parameter) {
   })
 }
 
+/**--------------资料库--------------- */
+/**常规物料 基本数据接口 */
+export function routineMaterialInfo(parameter) {
+  return axios({
+    baseURL: materialBaseUrl,
+    url: '/routineMaterialInfo',
+    method: 'GET',
+    params: parameter
+  })
+}
+/**常规物料新增 基本数据接口 */
+export function routineMaterialInfoAdd(parameter) {
+  return axios({
+    baseURL: materialBaseUrl,
+    url: '/routineMaterialInfo',
+    method: 'POST',
+    data: parameter
+  })
+}
+/**常规物料修改 基本数据接口 */
+export function routineMaterialInfoUpdate(parameter) {
+  return axios({
+    baseURL: materialBaseUrl,
+    url: '/routineMaterialInfo',
+    method: 'PUT',
+    data: parameter
+  })
+}
+/**常规物料删除 基本数据接口 */
+export function routineMaterialInfoDelete(parameter) {
+  let url = parameter.split('=')[1].split(',').map(s => `ids=${s}`).join('&')
+  return axios({
+    baseURL: materialBaseUrl,
+    url: `/routineMaterialInfo?${url}`,
+    method: 'DELETE'
+  })
+}
+/**反审核，权限码：routineMaterial:annulAudit*/
+export function routineMaterialInfoAnnulAudit(parameter) {
+  return axios({
+    baseURL: materialBaseUrl,
+    url: '/routineMaterialInfo/annulAudit',
+    method: 'PUT',
+    data: parameter
+  })
+}
+/**提交审核 */
+export function routineMaterialInfoAudit(parameter) {
+  return axios({
+    baseURL: materialBaseUrl,
+    url: '/routineMaterialInfo/audit',
+    method: 'PUT',
+    data: parameter
+  })
+}
+/**
+ * @description 判断是否有重复的物料代码，有返回true，没有返回false
+ * @param materialCode
+ */
+export function routineMaterialInfoCheckCode(parameter) {
+  return axios({
+    baseURL: materialBaseUrl,
+    url: '/routineMaterialInfo/checkCode',
+    method: 'GET',
+    params: parameter
+  })
+}
+/**
+ * @description 判断是否有重复的物料名称，有返回true，没有返回false
+ * @param materialName 物料名称
+ * @param ruleId 物料规则id
+ * @param id  物料id
+ */
+export function routineMaterialInfoCheckName(parameter) {
+  return axios({
+    baseURL: materialBaseUrl,
+    url: '/routineMaterialInfo/checkName',
+    method: 'GET',
+    params: parameter
+  })
+}
+/**
+ * @description 根据所选列表导出
+ * @param ids 所选的物料id
+ */
+ export function routineMaterialInfoExportList(parameter) {
+  return axios({
+    baseURL: materialBaseUrl,
+    url: '/routineMaterialInfo/exportList',
+    method: 'GET',
+    params: parameter
+  })
+}
+/**禁用 */
+export function routineMaterialInfoForbidden(parameter) {
+  return axios({
+    baseURL: materialBaseUrl,
+    url: '/routineMaterialInfo/forbidden',
+    method: 'PUT',
+    data: parameter
+  })
+}
+/**启用 */
+export function routineMaterialInfoStartUsing(parameter) {
+  return axios({
+    baseURL: materialBaseUrl,
+    url: '/routineMaterialInfo/startUsing',
+    method: 'PUT',
+    data: parameter
+  })
+}
+/**列表  */
+export function routineMaterialInfoPageList(parameter) {
+  return axios({
+    baseURL: materialBaseUrl,
+    url: '/routineMaterialInfo/pageList',
+    method: 'GET',
+    params: parameter
+  })
+}
+/**树形列表  */
+export function routineMaterialInfoTwoTierTreeList(parameter) {
+  return axios({
+    baseURL: materialBaseUrl,
+    url: '/routineMaterialInfo/twoTierTreeList',
+    method: 'GET',
+    params: parameter
+  })
+}
