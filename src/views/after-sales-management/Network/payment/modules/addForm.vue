fc3280d1e35938b8a9cb03b6bdeff1820f039217
xu520 <18260703019@163.com>
2021-10-23 11:35:42 +0800

    修改bug

--- src/views/after-sales-management/Network/payment/modules/addForm.vue
+++ src/views/after-sales-management/Network/payment/modules/addForm.vue
@@ -284,7 +284,7 @@ export default {
               paymentPostscript: values.paymentPostscript,
               screeningCost: values.screeningCost,
               troubleshootingCost: values.troubleshootingCost,
-              totalPayment: values.screeningCost + values.troubleshootingCost,
+              totalPayment: Number(values.screeningCost) + Number(values.troubleshootingCost),
             }
 
             _this.spinning = true
