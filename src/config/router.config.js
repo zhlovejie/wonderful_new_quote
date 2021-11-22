82216711d661f224c6543e5fc2759aa91350fa51
xu520 <18260703019@163.com>
2021-11-22 14:08:02 +0800

    修改采购模块不能打开问题

--- src/config/router.config.js
+++ src/config/router.config.js
@@ -64,6 +64,8 @@ const __ROUTER_MAP__ = [
   productProcessManagement, //工艺管理
   bomManagement, //BOM管理
   afterManagement, //售后管理
+  procurementModuleManagement, //采购管理
+  supplier, //供应商管理
 ]
 /*
 会有权限筛选处理，并作为菜单显示
