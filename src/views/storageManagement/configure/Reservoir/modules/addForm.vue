2c71cd506ba4d0026ccbf3b35e87e5b296982d48
xu520 <18260703019@163.com>
2022-01-18 15:47:02 +0800

    修改库区bug

--- src/views/storageManagement/configure/Reservoir/modules/addForm.vue
+++ src/views/storageManagement/configure/Reservoir/modules/addForm.vue
@@ -162,7 +162,7 @@ export default {
       this.visible = true
       this.addOredit = type
       this.record = record
-      this.reservoform.remark = ''
+      this.reservoform = {}
       getList().then((res) => {
         this.warehouseList = res.data
       })
