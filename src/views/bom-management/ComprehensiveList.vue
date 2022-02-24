dc6aad53d29b8b373e35f893d5f5085ec9264134
wangzhengzheng <bean_jie@aol.com>
2022-02-24 16:28:31 +0800

    fix bom综合 bom差异 树卡死问题

--- src/views/bom-management/ComprehensiveList.vue
+++ src/views/bom-management/ComprehensiveList.vue
@@ -681,7 +681,7 @@ export default {
         }
         let {isRule,isProduct,isSubProduct} = treeNode.dataRef
         if(isRule){
-          let ruleResult = await productMaterialInfoTwoTierTreeList({ parentId: treeNode.dataRef.value }).then(res => res.data).catch(err => {
+          let ruleResult = await productMaterialInfoTwoTierTreeList({ parentId: treeNode.dataRef.value }).then(res => res.data || []).catch(err => {
             console.log(err)
             return []
           })
@@ -689,7 +689,7 @@ export default {
             treeNode.dataRef.children = ruleResult.map(item => that.formatRuleNode(item))
             that.orgTree = [...that.orgTree]
           }else{
-            let productResult = await getAllProductMaterial({ruleId:treeNode.dataRef.value}).then(res => res.data).catch(err => {
+            let productResult = await getAllProductMaterial({ruleId:treeNode.dataRef.value}).then(res => res.data || []).catch(err => {
             console.log(err)
             return []
           })
@@ -700,7 +700,7 @@ export default {
           }
         }
         if(isProduct){
-          let subProductResult = await allListMaterialForm({groupId:treeNode.dataRef.__id}).then(res => res.data).catch(err => {
+          let subProductResult = await allListMaterialForm({groupId:treeNode.dataRef.__id}).then(res => res.data || []).catch(err => {
             console.log(err)
             return []
           })
